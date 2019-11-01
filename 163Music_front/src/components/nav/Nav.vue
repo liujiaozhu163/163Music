f91a645a4ebc5ab760a689fd8ca70af3f748ac9a
wenyixuan <2681379356@example.com>
2019-11-01 11:56:17 +0800

    修改了Nav

--- 163Music_front/src/components/nav/Nav.vue
+++ 163Music_front/src/components/nav/Nav.vue
@@ -1,8 +1,8 @@
 <template>
 
   <div>
-   <router-link to="/">首页</router-link>
-   <router-link to="/topList">排行榜</router-link>
+   <router-link to="/">首页123134</router-link>
+   <router-link to="/topList">排行榜23156465</router-link>
    <router-link to="/personalized">歌单</router-link>
    <router-link to="/dj">主播电台</router-link>
    <router-link to="/artist">歌手</router-link>
