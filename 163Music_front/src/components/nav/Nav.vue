fec31f004b2abbc7b7bac4491a9c33eabcf7f492
liujiaozhu163 <liujiaozhu666@163.com>
2019-11-01 11:25:17 +0800

    修改了nav

--- 163Music_front/src/components/nav/Nav.vue
+++ 163Music_front/src/components/nav/Nav.vue
@@ -1,17 +1,17 @@
 <template>
-  
+
   <div>
-   <router-link to="/">首页</router-link>
-   <router-link to="/topList">排行榜</router-link>
-   <router-link to="/personalized">歌单</router-link>
-   <router-link to="/dj">主播电台</router-link>
-   <router-link to="/artist">歌手</router-link>
-   <router-link to="/album">新碟上架</router-link>
+   <router-link to="/">首页111</router-link>
+   <router-link to="/topList">排行榜111</router-link>
+   <router-link to="/personalized">歌单111</router-link>
+   <router-link to="/dj">主播电台11</router-link>
+   <router-link to="/artist">歌手11</router-link>
+   <router-link to="/album">新碟上架11</router-link>
   </div>
 </template>
 
 <script>
-  
+
   export default {
     name:'nav'
   }
